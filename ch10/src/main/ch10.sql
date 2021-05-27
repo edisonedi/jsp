4eaa74d54931daedd44308268a9eab485c91340e
DESKTOP-9MD0DC9\ediso <edison1115@naver.com>
2021-05-27 15:16:00 +0900

    ch10

---
+++ ch10/src/main/ch10.sql
@@ -0,0 +1,10 @@
+create table member2(
+	 id varchar2(12) primary key,
+	 password varchar2(12) not null,
+	 name varchar2(20) not null,
+	 address varchar2(50),
+	 tel varchar2(20) not null,
+	 reg_date date
+);
+alter table member2 add(del char(1));
+update member2 set del='n';
